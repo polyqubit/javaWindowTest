04ce0fc1c6c856d09a2788018a9b91744c4daecf
polyqubit <polyumbrial@gmail.com>
2021-10-22 20:23:34 -0700

    Update starter.java

--- starter.java
+++ starter.java
@@ -11,7 +11,7 @@ public class starter {
 			frameSet[i].setSize(400,400);
 			frameSet[i].setLocation(rand.nextInt(3800)-30,rand.nextInt(1000)-30);
 			frameSet[i].setResizable(false);
-			frameSet[i].add(new JLabel(new ImageIcon("C:/Users/polyu/OneDrive/Desktop/UniTest/HOO.png")));
+			frameSet[i].add(new JLabel(new ImageIcon("C:/Users/polyu/OneDrive/Desktop/UniTest/HOO.png")));  //replace the path with your own path
 			frameSet[i].setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 			frameSet[i].setAlwaysOnTop(true);
 			frameSet[i].setVisible(true);
